30e86e2055a024b8a1279994bdcb9d0e294b20c0
alvindrakes <alvinlohjinxian@gmail.com>
2018-10-13 15:04:52 +0800

    OCR camera working now

--- app/src/main/java/com/example/alvindrakes/doubleparkingapp/HomePage.java
+++ app/src/main/java/com/example/alvindrakes/doubleparkingapp/HomePage.java
@@ -1,8 +1,16 @@
 package com.example.alvindrakes.doubleparkingapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private static final int RC_OCR_CAPTURE = 9003;
+    private static final String TAG = "HomePage";
+
+    private String textRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +56,17 @@ public class HomePage extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // for scanNumberPlateBtn
+        Button scanPlateNumberBtn = (Button) findViewById(R.id.scanPlateNumber);
+
+        scanPlateNumberBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ocrIntent = new Intent(HomePage.this, OCR_reader.class);
+                startActivityForResult(ocrIntent, RC_OCR_CAPTURE);
+            }
+        });
     }
 
     @Override
@@ -81,5 +116,29 @@ public class HomePage extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    //----------------------------------------------------------------
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RC_OCR_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String text = data.getStringExtra(OCR_reader.TextBlockObject);
+                    Toast.makeText(HomePage.this, text,
+                            Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Text read: " + text);
+                } else {
+                    Log.d(TAG, "No Text captured, intent data is null");
+                }
+            } else {
+////                statusMessage.setText(String.format(getString(R.string.ocr_error),
+//                        CommonStatusCodes.getStatusCodeString(resultCode)));
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
