eed64997bc94e685bb81e9f39f78ee71827de369
superlws1 <superlws1@hotmail.com>
2018-10-13 16:21:10 +0800

    signup (gave up)

--- app/src/main/java/com/example/alvindrakes/doubleparkingapp/User.java
+++ app/src/main/java/com/example/alvindrakes/doubleparkingapp/User.java
@@ -10,13 +10,22 @@ import java.util.List;
 import java.util.Map;
 
 public class User {
+    private String name;
     private int phoneNumber;
     private List<String> carNumber;
 
-
-    public User(int phoneNumber, List<String> carNumber) {
+    public User(String name, int phoneNumber, List<String> carNumber) {
+        this.name = name;
         this.phoneNumber = phoneNumber;
         this.carNumber = carNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getPhoneNumber() {
@@ -27,20 +36,24 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    public List<String> getCarNumber() {
+        return carNumber;
+    }
+
     public void setCarNumber(List<String> carNumber) {
         this.carNumber = carNumber;
     }
 
-    public static void insertCar(List<String> carNumber){
+    public static void insertCar(User user){
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
         Map<String,Object> updates = new HashMap<>();
-        updates.put(firebaseUser.getPhoneNumber(), carNumber);
+        updates.put(firebaseUser.getPhoneNumber(), user.carNumber);
 
         int carIndex = 1;
-        while (!carNumber.isEmpty()) {
-            updates.put(firebaseUser.getPhoneNumber() + "/car " + carIndex + "/", carNumber.remove(0));
+        while (!user.carNumber.isEmpty()) {
+            updates.put(firebaseUser.getPhoneNumber() + "/car " + carIndex + "/", user.carNumber.remove(0));
             carIndex++;
         }
         databaseReference.updateChildren(updates);
